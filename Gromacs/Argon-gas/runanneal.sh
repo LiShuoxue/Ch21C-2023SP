483d685b59784dca8e08cf1ad441eff49c8333d2
LiShuoxue <sli7@caltech.edu>
2023-04-03 09:33:40 -0700

    update docs

--- Gromacs/Argon-gas/runanneal.sh
+++ Gromacs/Argon-gas/runanneal.sh
@@ -7,13 +7,13 @@ curl -O "${filepath}argon.top"
 curl -O "${filepath}argon_start.pdb"
 curl -O "${filepath}md.mdp"
 cat md.mdp | sed "s/MYTEMP/$1/g" > mdwork.mdp
-curl -O "${filepath}anneal.mdp" > anneal-origin.mdp
-cat anneal-origin.mdp | sed "s/STARTTEMP/$1/g" | sed "s/ENDTEMP/$2/g" > anneal.mdp
+curl -O "${filepath}anneal.mdp"
+cat anneal.mdp | sed "s/STARTTEMP/$1/g" | sed "s/ENDTEMP/$2/g" > anneal-work.mdp
 
 gmx grompp -f mdwork.mdp -c argon_start.pdb -p argon.top
 gmx mdrun -s topol.tpr -v -c argon_1ns.gro -nice 0
 mv traj_comp.xtc gas.xtc
-gmx grompp -f anneal.mdp -c argon_start.pdb -p argon.top -maxwarn 1
+gmx grompp -f anneal-work.mdp -c argon_start.pdb -p argon.top -maxwarn 1
 
 # Post-processing
 echo 4  | gmx energy -o Potential.xvg
